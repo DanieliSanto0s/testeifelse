eb48ed0176d5dbf6318d13eac5c5a97ac3b35a76
DanieliSanto0s <danielivitoria93taqu@gmail.com>
2021-10-02 17:26:02 -0300

    teste de ifelse

---
+++ ifelse/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          string mes;
+          Console.WriteLine("Digite um mês:");
+          mes = Console.ReadLine();
+
+          if (mes == "janeiro")
+          {
+            Console.WriteLine("Esse mês tem 31 dias");
+          }
+
+          else if (mes == "fevereiro")
+          {
+            Console.WriteLine("Esse mês tem 28 ou 29 dias");
+          }
+
+          else if (mes == "março")
+          {
+            Console.WriteLine("Esse mês tem 31 dias");
+          }
+
+          else if (mes == "maio")
+          {
+            Console.WriteLine("Esse mês tem 31 dias");
+          }
+
+          else if (mes == "julho")
+          {
+            Console.WriteLine("Esse mês tem 31 dias");
+          }
+
+          else if (mes == "agosto")
+          {
+            Console.WriteLine("Esse mês tem 31 dias");
+          }
+
+          else if (mes == "outubro")
+          {
+            Console.WriteLine("Esse mês tem 31 dias");
+          }
+
+          else if (mes == "dezembro")
+          {
+            Console.WriteLine("Esse mês tem 31 dias");
+          }
+
+           else
+          {
+            Console.WriteLine("Esse mês tem 30 dias");
+          }
+ 
+        }
+    }
+}
